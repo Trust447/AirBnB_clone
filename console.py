4c804cedc7708e307e2858637af9d4b5fa2263ae
trust <isaactrust447@gmail.com>
2023-11-12 23:01:07 +0100

    fixing task 6

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
-m models.state import State
+from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
