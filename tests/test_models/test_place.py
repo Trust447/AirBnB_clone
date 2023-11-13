3e1689d364f7aff1e7361d0d9695d48e91f1eb92
trust <isaactrust447@gmail.com>
2023-11-13 22:41:08 +0100

    fixed error related filestorage

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """unittests for models/user.py"""
 import unittest
-from models.user import Place
+from models.place import Place
 
 
 class TestPlace(unittest.TestCase):
-    place = Place()
-    self.assertEqual(place.city_id, "")
-    self.assertEqual(place.user_id, "")
-    self.assertEqual(place.name, "")
-    self.assertEqual(place.description, "")
-    self.assertEqual(place.number_rooms, 0)
-    self.assertEqual(place.number_bathrooms, 0)
-    self.assertEqual(place.max_guest, 0)
-    self.assertEqual(place.price_by_night, 0)
-    self.assertEqual(place.latitude, 0.0)
-    self.assertEqual(place.longitude, 0.0)
-    self.assertEqual(place.amenity_ids, [])
+
+    def test_place_attributes(self):
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
 
 if __name__ == '__main__':
     unittest.main()
