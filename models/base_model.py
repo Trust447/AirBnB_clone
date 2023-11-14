e08d4b98ff2b94b1503c485bc87054adfaa9170e
trust <isaactrust447@gmail.com>
2023-11-14 04:30:40 +0100

    fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,6 @@ class BaseModel:
         """constructor for BaseModel arguments"""
         """from models import storage"""
 
-        from models import storage
         self.id = kwargs.get('id', str(uuid.uuid4()))
         self.created_at = kwargs.get('created_at', datetime.now())
         self.updated_at = kwargs.get('updated_at', datetime.now())
@@ -43,6 +42,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime"""
+        from models import storage
 
         self.updated_at = datetime.now()
         storage.save()
