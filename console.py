ef12d0358830586f83cceb09eeec4569f520e09e
trust <isaactrust447@gmail.com>
2023-11-14 06:06:38 +0100

    fixing bugs

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models import storage
+""" module for the Aibnb clone console"""
 
 class HBNBCommand(cmd.Cmd):
     """Interpreter for HBNB Command program"""
